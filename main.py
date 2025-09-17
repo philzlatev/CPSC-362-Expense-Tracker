45ae5dbdee2809f16efda475f061632c1f731d7f
AffanSdq <affan.sdq@gmail.com>
2025-09-17 09:34:15 -0700

    Basic Visualizations

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import statsmodels.api as sm
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 pd.plotting.register_matplotlib_converters()
 print("Setup Complete")
 
 
 # Assuming data is in csv file
-file_path = "/Users/suhailkhaled/CPSC-362-Expense-Tracker/expenses.csv"
-
+file_path = os.path.join(os.path.dirname(__file__), "expenses.csv")
 df = pd.read_csv(file_path)
 
 sns.lineplot(data=df, x="date", y="amount", hue="category")

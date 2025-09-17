138161866e71bf8dc39fef923ab36e018d40aeba
Suhail Khaled <suhailkhaled@Suhails-MacBook-Air-8.local>
2025-09-17 08:54:24 -0700

    Setup complete

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import numpy as np
+import statsmodels.api as sm
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 
+pd.plotting.register_matplotlib_converters()
+print("Setup Complete")
+
+
+# Assuming data is in csv file
+file_path = "/Users/suhailkhaled/CPSC-362-Expense-Tracker/expenses.csv"
+
+df = pd.read_csv(file_path)
+print(df)

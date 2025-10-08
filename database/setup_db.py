3ad2c8e7c08ed4beb246c400f33e5f0a57176f71
AffanSdq <affan.sdq@gmail.com>
2025-10-07 19:14:45 -0700

    Fixing issue with amount column and id assignment

--- database/setup_db.py
+++ database/setup_db.py
@@ -5,7 +5,7 @@ cursor = conn.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY, /*can add AUTOINCREMENT*/
     name TEXT NOT NULL,
     salary INTEGER
 );
@@ -13,13 +13,13 @@ CREATE TABLE IF NOT EXISTS users (
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS transactions (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    amount REAL NOT NULL,
-    category TEXT NOT NULL,
-    merchant TEXT,
-    user_id INTEGER NOT NULL,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+  id INTEGER PRIMARY KEY, /*can add AUTOINCREMENT*/
+  user_id INTEGER NOT NULL,
+  amount REAL NOT NULL CHECK (amount >= 0),
+  category TEXT NOT NULL CHECK (trim(category) <> ''),
+  merchant TEXT,
+  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 """)
 

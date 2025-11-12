226c651d3868917b03b39801cd2d2fa123715382
Mary6573 <89876301+Mary6573@users.noreply.github.com>
2025-11-12 11:30:20 -0800

    Add files via upload

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TrackStack.Data;
+using TrackStack.Models;
+
+namespace TrackStack.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExpensesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Expenses
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Expenses.ToListAsync());
+        }
+        // GET: Expenses/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+        //GET: Expenses/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            return View("Index",await _context.Expenses.Where(j => j.Description.Contains(SearchPhrase)).ToListAsync());
+        }
+
+        // GET: Expenses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenses = await _context.Expenses
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (expenses == null)
+            {
+                return NotFound();
+            }
+
+            return View(expenses);
+        }
+
+        // GET: Expenses/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Expenses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Amount,Type,Description")] Expenses expenses)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(expenses);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expenses);
+        }
+
+        // GET: Expenses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenses = await _context.Expenses.FindAsync(id);
+            if (expenses == null)
+            {
+                return NotFound();
+            }
+            return View(expenses);
+        }
+
+        // POST: Expenses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Amount,Type,Description")] Expenses expenses)
+        {
+            if (id != expenses.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(expenses);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExpensesExists(expenses.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expenses);
+        }
+
+        // GET: Expenses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenses = await _context.Expenses
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (expenses == null)
+            {
+                return NotFound();
+            }
+
+            return View(expenses);
+        }
+
+        // POST: Expenses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var expenses = await _context.Expenses.FindAsync(id);
+            if (expenses != null)
+            {
+                _context.Expenses.Remove(expenses);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExpensesExists(int id)
+        {
+            return _context.Expenses.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Models/Expenses.cs
@@ -0,0 +1,16 @@
+﻿namespace TrackStack.Models
+{
+    public class Expenses
+    {
+        public int ID { get; set; }
+        public int Amount { get; set; }
+        public int Type { get; set; }
+
+        public string Description { get; set; }
+
+        public Expenses()
+        {
+            
+        }
+    }
+}

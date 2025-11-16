3642be32b2a5677c9601aed871f3d895912bcc5d
AffanSdq <139398751+AffanSdq@users.noreply.github.com>
2025-11-16 15:13:58 -0800

    Add User model with properties Id, Name, and Salary

---
+++ TrackStack/Models/User.cs
@@ -0,0 +1,9 @@
+namespace TrackStack.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}

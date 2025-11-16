1d14564415d2c24ce1d7f64986e57d9d6416688b
AffanSdq <139398751+AffanSdq@users.noreply.github.com>
2025-11-16 15:52:26 -0800

    Refactor ApplicationDbContext to use IdentityUser
    Updated ApplicationDbContext to use IdentityUser type and configured Identity properties for SQLite compatibility.

--- TrackStack/Data/ApplicationDbContext.cs
+++ TrackStack/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TrackStack.Models;
 
 namespace TrackStack.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -14,5 +15,32 @@ namespace TrackStack.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<Expenses> Expenses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Make Identity columns SQLite-compatible
+            builder.Entity<IdentityUser>(b =>
+            {
+                b.Property(u => u.Id).HasColumnType("TEXT");
+            });
+
+            builder.Entity<IdentityRole>(b =>
+            {
+                b.Property(r => r.Id).HasColumnType("TEXT");
+                b.Property(r => r.ConcurrencyStamp).HasColumnType("TEXT");
+            });
+
+            builder.Entity<IdentityUserLogin<string>>(b =>
+            {
+                b.Property(l => l.ProviderKey).HasColumnType("TEXT");
+            });
+
+            builder.Entity<IdentityUserToken<string>>(b =>
+            {
+                b.Property(t => t.Value).HasColumnType("TEXT");
+            });
+        }
     }
 }

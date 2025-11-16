aeca0f8440cc08c5d60721da035261001f69b298
AffanSdq <139398751+AffanSdq@users.noreply.github.com>
2025-11-16 15:24:49 -0800

    Add Expenses DbSet to ApplicationDbContext

--- TrackStack/Data/ApplicationDbContext.cs
+++ TrackStack/Data/ApplicationDbContext.cs
@@ -13,5 +13,6 @@ namespace TrackStack.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Expenses> Expenses { get; set; }
     }
 }

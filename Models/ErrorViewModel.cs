f230803f016851b14b213f119bb3d6e4f636b2d9
AffanSdq <affan.sdq@gmail.com>
2025-11-19 10:08:35 -0800

    Cleaned up

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TrackStack.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
